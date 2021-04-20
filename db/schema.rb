2a381127a46620683e573a9e827b14cabcc73d2f
Amanda <ajnikrant@gmail.com>
2021-04-20 10:36:20 -0400

    added name to listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_181423) do
+ActiveRecord::Schema.define(version: 2021_04_20_143533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_04_19_181423) do
     t.string "host_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
