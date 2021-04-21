d6e70febbd0f286f743250072cfee0829b70421e
Susana <susana.m.vik@gmail.com>
2021-04-21 17:08:59 -0400

    conflict resolution

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -34,5 +34,5 @@ private
     def review_params
         params.require(:review).permit(:rating, :comment)
     end
-    
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,5 @@ class Listing < ApplicationRecord
     has_many :bookings
     has_many :guests, through: :bookings
     belongs_to :host, optional: true
-
+    has_many :reviews
 end
